3570e7528fa66bec9bed587d498241ac80a391bc
Jared E <jaredeic@yahoo.com>
2023-10-23 12:10:59 -0700

    finished writing javascript for password generator

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -8,16 +9,47 @@ function writePassword() {
 
 }
 
-function generatePassword() {
+
+  function generatePassword() {
     var length = prompt ("Enter the length of the password (between 8 and 128 characters):");
     if (length < 8) {
         alert("Please enter a number greater than 7.")
     } else if (length > 128) {
         alert("Please enter a number less than 129.")
-    } else if (isNaN(length)) {
+    } else (isNaN(length)) 
         alert("Please enter a valid number between 8 and 128.");
-    }
-    return '';
+    return "";
+
+
+  var includeLowercase = confirm("Do you want to include lowercase characters?");
+  var includeUppercase = confirm("Do you want to include uppercase characters?");
+  var includeNumeric = confirm("Do you want to include numeric characters?");
+  var includeSpecial = confirm("Do you want to include special characters?");
+
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("You must select at least one character type.");
+    return "";
+  }
+
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+  var allChars = "";
+  if (includeLowercase) allChars += lowercaseChars;
+  if (includeUppercase) allChars += uppercaseChars;
+  if (includeNumeric) allChars += numericChars;
+  if (includeSpecial) allChars += specialChars;
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+  }
+
+  return password;
 }
+
 
 generateBtn.addEventListener("click", writePassword);
